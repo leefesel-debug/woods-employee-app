cc9da8f78b273fe8ec413c53e05ecafc0baa8cfc
leefesel-debug <leefesel@gmail.com>
2026-09-19 17:35:15 +0100

    Add safe supplier order test mode

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.WOODS_CONFIG = {
   supabaseUrl: "https://ypquqxolphrocdohcrrg.supabase.co",
   supabaseAnonKey: "sb_publishable_XgzcSnuaGVJcpzWYbIjr7Q_ibysLKJa",
-  secureOrderEmail: true
+  secureOrderEmail: true,
+  orderEmailTestMode: true
 };
