58ad37e2a81bba9bcadb87d7a5a6ab6bcf0b518e
leefesel-debug <leefesel@gmail.com>
2026-09-19 17:29:59 +0100

    Enable secure Resend supplier order emails

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.WOODS_CONFIG = {
   supabaseUrl: "https://ypquqxolphrocdohcrrg.supabase.co",
-  supabaseAnonKey: "sb_publishable_XgzcSnuaGVJcpzWYbIjr7Q_ibysLKJa"
+  supabaseAnonKey: "sb_publishable_XgzcSnuaGVJcpzWYbIjr7Q_ibysLKJa",
+  secureOrderEmail: true
 };
