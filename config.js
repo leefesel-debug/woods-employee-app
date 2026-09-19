7fc15506dcd5f945f9ec012461fb32359c447829
leefesel-debug <leefesel@gmail.com>
2026-09-19 17:53:31 +0100

    Go live with supplier order emails

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.WOODS_CONFIG = {
   supabaseUrl: "https://ypquqxolphrocdohcrrg.supabase.co",
   supabaseAnonKey: "sb_publishable_XgzcSnuaGVJcpzWYbIjr7Q_ibysLKJa",
   secureOrderEmail: true,
-  orderEmailTestMode: true
+  orderEmailTestMode: false
 };
